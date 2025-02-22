7d4a5a637672fdd1e31536b1a44f45bd18a87560
Sanaz Hosseini <sanazhosseini73@outlook.com>
2025-02-23 00:54:45 +0100

    💎style: enhance button

--- src/components/ui/button/button.type.ts
+++ src/components/ui/button/button.type.ts
@@ -3,6 +3,11 @@ interface IButtonProps {
     onClick?: () => void
     className?: string
     type?: "button" | "submit" | "reset"
+    variant?: "primary" | "secondary" | "muted"
+    size?: "small" | "medium" | "large"
+    disabled?: boolean
+    loading?: boolean
+    fullWidth?: boolean
 }
 
 export default IButtonProps
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,27 @@ const config: Config = {
         './src/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'primary': '#1c398e',
+                'secondary': '#162455',
+            },
+            spacing: {
+                '0': '0px',
+                '1': '8px',
+                '2': '16px',
+                '3': '24px',
+                '4': '32px',
+                '5': '40px',
+                '6': '48px',
+                '7': '56px',
+                '8': '64px',
+                '9': '72px',
+                '10': '80px',
+                '11': '88px',
+                '12': '96px',
+            }
+        },
     },
     plugins: [],
 };
