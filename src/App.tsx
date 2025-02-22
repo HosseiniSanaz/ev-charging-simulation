203923d58d390bdfbf07745a51865ef047df135d
Sanaz Hosseini <sanazhosseini73@outlook.com>
2025-02-22 20:11:18 +0100

    🚀init: create base project and add tailwind and rechart

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className="p-4 bg-blue-500 text-white">
+      <h1 className="text-2xl font-bold">Hello, World!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
